52e2fbe2adb4e4f575b6a0789ec1f2ac52798c24
JYP2098 <pateljay2098@gmail.com>
2020-10-08 11:20:13 +0530

    v1

---
+++ App.js
@@ -0,0 +1,17 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import ChatScreen from "./screens/ChatScreen";
+
+import LoginScreen from "./screens/LoginScreen";
+
+const AppNavigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Chat: ChatScreen,
+  },
+  {
+    headerMode: "none",
+  }
+);
+
+export default createAppContainer(AppNavigator);
